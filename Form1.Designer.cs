e4b097408674d0d6ab3166703bacc9bcd337f779
Jenetics86 <jenniferdee86@gmail.com>
2021-10-25 21:50:47 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace lotto_quickPick
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Btn_pick = new System.Windows.Forms.Button();
+            this.Btn_reset = new System.Windows.Forms.Button();
+            this.Line1 = new System.Windows.Forms.TextBox();
+            this.Line2 = new System.Windows.Forms.TextBox();
+            this.Line3 = new System.Windows.Forms.TextBox();
+            this.Line4 = new System.Windows.Forms.TextBox();
+            this.Line5 = new System.Windows.Forms.TextBox();
+            this.Line6 = new System.Windows.Forms.TextBox();
+            this.Line7 = new System.Windows.Forms.TextBox();
+            this.Line8 = new System.Windows.Forms.TextBox();
+            this.Line9 = new System.Windows.Forms.TextBox();
+            this.Line10 = new System.Windows.Forms.TextBox();
+            this.CostBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btn_pick
+            // 
+            this.Btn_pick.BackColor = System.Drawing.Color.Purple;
+            this.Btn_pick.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn_pick.Font = new System.Drawing.Font("Book Antiqua", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.Btn_pick.Location = new System.Drawing.Point(38, 429);
+            this.Btn_pick.Name = "Btn_pick";
+            this.Btn_pick.Size = new System.Drawing.Size(137, 94);
+            this.Btn_pick.TabIndex = 0;
+            this.Btn_pick.Text = "Quick pick";
+            this.Btn_pick.UseVisualStyleBackColor = false;
+            this.Btn_pick.Click += new System.EventHandler(this.Btn_pick_Click);
+            // 
+            // Btn_reset
+            // 
+            this.Btn_reset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Btn_reset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn_reset.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Btn_reset.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Btn_reset.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.Btn_reset.Font = new System.Drawing.Font("Book Antiqua", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.Btn_reset.Location = new System.Drawing.Point(203, 433);
+            this.Btn_reset.Name = "Btn_reset";
+            this.Btn_reset.Size = new System.Drawing.Size(137, 93);
+            this.Btn_reset.TabIndex = 1;
+            this.Btn_reset.Text = "Reset";
+            this.Btn_reset.UseVisualStyleBackColor = false;
+            this.Btn_reset.Click += new System.EventHandler(this.Btn_reset_Click);
+            // 
+            // Line1
+            // 
+            this.Line1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Line1.Location = new System.Drawing.Point(628, 202);
+            this.Line1.Name = "Line1";
+            this.Line1.Size = new System.Drawing.Size(204, 27);
+            this.Line1.TabIndex = 2;
+            // 
+            // Line2
+            // 
+            this.Line2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Line2.Location = new System.Drawing.Point(628, 235);
+            this.Line2.Name = "Line2";
+            this.Line2.Size = new System.Drawing.Size(204, 27);
+            this.Line2.TabIndex = 3;
+            // 
+            // Line3
+            // 
+            this.Line3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Line3.Location = new System.Drawing.Point(628, 268);
+            this.Line3.Name = "Line3";
+            this.Line3.Size = new System.Drawing.Size(204, 27);
+            this.Line3.TabIndex = 4;
+            // 
+            // Line4
+            // 
+            this.Line4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Line4.Location = new System.Drawing.Point(628, 301);
+            this.Line4.Name = "Line4";
+            this.Line4.Size = new System.Drawing.Size(204, 27);
+            this.Line4.TabIndex = 5;
+            // 
+            // Line5
+            // 
+            this.Line5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Line5.Location = new System.Drawing.Point(628, 334);
+            this.Line5.Name = "Line5";
+            this.Line5.Size = new System.Drawing.Size(204, 27);
+            this.Line5.TabIndex = 6;
+            // 
+            // Line6
+            // 
+            this.Line6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Line6.Location = new System.Drawing.Point(628, 367);
+            this.Line6.Name = "Line6";
+            this.Line6.Size = new System.Drawing.Size(204, 27);
+            this.Line6.TabIndex = 7;
+            // 
+            // Line7
+            // 
+            this.Line7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Line7.Location = new System.Drawing.Point(628, 400);
+            this.Line7.Name = "Line7";
+            this.Line7.Size = new System.Drawing.Size(204, 27);
+            this.Line7.TabIndex = 8;
+            // 
+            // Line8
+            // 
+            this.Line8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Line8.Location = new System.Drawing.Point(628, 433);
+            this.Line8.Name = "Line8";
+            this.Line8.Size = new System.Drawing.Size(204, 27);
+            this.Line8.TabIndex = 9;
+            // 
+            // Line9
+            // 
+            this.Line9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Line9.Location = new System.Drawing.Point(628, 466);
+            this.Line9.Name = "Line9";
+            this.Line9.Size = new System.Drawing.Size(204, 27);
+            this.Line9.TabIndex = 10;
+            // 
+            // Line10
+            // 
+            this.Line10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Line10.Location = new System.Drawing.Point(628, 499);
+            this.Line10.Name = "Line10";
+            this.Line10.Size = new System.Drawing.Size(204, 27);
+            this.Line10.TabIndex = 11;
+            // 
+            // CostBox
+            // 
+            this.CostBox.Location = new System.Drawing.Point(486, 499);
+            this.CostBox.Name = "CostBox";
+            this.CostBox.Size = new System.Drawing.Size(125, 27);
+            this.CostBox.TabIndex = 15;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(866, 195);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(868, 543);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CostBox);
+            this.Controls.Add(this.Line10);
+            this.Controls.Add(this.Line9);
+            this.Controls.Add(this.Line8);
+            this.Controls.Add(this.Line7);
+            this.Controls.Add(this.Line6);
+            this.Controls.Add(this.Line5);
+            this.Controls.Add(this.Line4);
+            this.Controls.Add(this.Line3);
+            this.Controls.Add(this.Line2);
+            this.Controls.Add(this.Line1);
+            this.Controls.Add(this.Btn_reset);
+            this.Controls.Add(this.Btn_pick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_pick;
+        private System.Windows.Forms.Button Btn_reset;
+        private System.Windows.Forms.TextBox Line1;
+        private System.Windows.Forms.TextBox Line2;
+        private System.Windows.Forms.TextBox Line3;
+        private System.Windows.Forms.TextBox Line4;
+        private System.Windows.Forms.TextBox Line5;
+        private System.Windows.Forms.TextBox Line6;
+        private System.Windows.Forms.TextBox Line7;
+        private System.Windows.Forms.TextBox Line8;
+        private System.Windows.Forms.TextBox Line9;
+        private System.Windows.Forms.TextBox Line10;
+        private System.Windows.Forms.TextBox CostBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lotto_quickPick
+{
+    
+    public partial class Form1 : Form
+    {   
+        int[] LottoNum = new int[47]; //creates array to hold numbers 1-47.
+        int Btnclicks = 0;
+        int Reset_Btnclicks = 0;
+        int cost = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            Btn_reset.Enabled = false;
+        }
+
+        private void clear()
+        {
+            Line1.Text = "";
+            Line2.Text = "";
+            Line3.Text = "";
+            Line4.Text = "";
+            Line5.Text = "";
+            Line6.Text = "";
+            Line7.Text = "";
+            Line8.Text = "";
+            Line9.Text = "";
+            Line10.Text = "";
+            Btn_pick.Enabled = true;
+            
+        }
+        private void Btn_reset_Click(object sender, EventArgs e)
+        {
+            Reset_Btnclicks++;
+            if (Reset_Btnclicks > 0)
+            {
+                Btn_reset.Enabled = false;
+            }
+            clear();
+            
+        }
+
+        private void Btn_pick_Click(object sender, EventArgs e)
+        {
+            Btnclicks++;
+            Random rnd = new Random(); //creates random number generator object.
+            
+            for (int i = 1; i < LottoNum.Length; i++) //if set to zero goes outside scope of array and breaks, set to 1 works(find out why).
+
+                LottoNum[i - 1] = i; //should fill array "lottoNum" indexes 0-46 with 47 numbers. 
+
+            for (int i = 1; i < LottoNum.Length; i++)//fisher - yates shuffle
+            {
+                int j = rnd.Next() % LottoNum.Length; //get random number assign to j
+                int position = LottoNum[i]; //position = the position of lottoNum[i]
+                LottoNum[i] = LottoNum[j]; //lottoNum[i] swapped to lottoNum[j]
+                LottoNum[j] = position;  //lottoNum[j] now has lottoNum[i]'s original position 
+            }
+
+            //foreach (int number in Enumerable.Range(1, 47).OrderBy(x => rnd.Next()))
+            //{
+            //    Line1.Text = string.Join("", number, " ", rnd.Next(number), " ", rnd.Next(number), " ", rnd.Next(number), " ", rnd.Next(number), " ", rnd.Next(number));
+            //} // not good has repeating numbers
+
+            /*----- this is where i'm trying to get a single button cycle through the sequence
+                    and populate the text box with 6 additional random numbers*/
+
+            for (int count = 1; count < 7; count++) // count is each number on the line, if less than 7 (or == 6) 
+            {
+                
+                switch (Btnclicks)//if Btnclick = 1 fill textBox 1 etc.
+                {
+                    case 1:
+                        //Line1.Text = string.Join("", LottoNum[0], "  ", LottoNum[1], "  ", LottoNum[2], "  ", LottoNum[3], "  ", LottoNum[4], "  ", LottoNum[5] );
+                        Line1.Text = Line1.Text + string.Join("", LottoNum[count], "  "); 
+                        break;
+
+                    case 2:
+                        Line2.Text += string.Join("", LottoNum[count], "  "); // += lets the entire string appear in the box instead of just the last index
+                        break;
+
+                    case 3:
+                        Line3.Text += string.Join("", LottoNum[count], "  ");
+                        break;
+
+                    case 4:
+                        Line4.Text += string.Join("", LottoNum[count], "  ");
+                        break;
+
+                    case 5:
+                        Line5.Text += string.Join("", LottoNum[count], "  "); 
+                        break;
+
+                    case 6:
+                        Line6.Text += string.Join("", LottoNum[count], "  "); 
+                        break;
+
+                    case 7:
+                        Line7.Text += string.Join("", LottoNum[count], "  "); 
+                        break;
+
+                    case 8:
+                        Line8.Text += string.Join("", LottoNum[count], "  "); 
+                        break;
+
+                    case 9:
+                        Line9.Text += string.Join("", LottoNum[count], "  ");
+                        break;
+
+                    case 10:
+                        Line10.Text += string.Join("", LottoNum[count], "  "); 
+                        break;
+                };
+
+                if (Btnclicks > 10)
+                {
+                    MessageBox.Show("You have 10 Quick Picks, please reset to create more lines");
+                    Btnclicks = 0; //if the button clicks > than 10, resets button clicks to 0 to allow to repopulate the table after reset
+                    Btn_pick.Enabled = false;
+                    Btn_reset.Enabled = true;
+                }
+
+            }
+        }
+    }
+}
